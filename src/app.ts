5ab9988581963da78426df276317b1d105c37f51
Md Ridoan <ridoan437@gmail.com>
2026-05-27 00:14:28 +0600

    not found middleware and global error handler added

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,10 @@
 import express, { Application, Request, Response } from "express";
 import { prisma } from "./app/lib/prisma";
 import { IndexRoutes } from "./app/routes";
+import { globalErrorHandler } from "./app/middlewares/globalErrorHandler";
+import { notFound } from "./app/middlewares/notFound";
 
 const app: Application = express();
-
 
 // Enable URL-encoded form data parsing
 app.use(express.urlencoded({ extended: true }));
@@ -14,18 +15,20 @@ app.use(express.json());
 app.use("/api/v1", IndexRoutes);
 
 // Basic route
-app.get('/', async (req: Request, res: Response) => {
-
-    const specialty = await prisma.specialty.create({
-        data: {
-            title: 'Cardiology'
-        }
-    })
-    res.status(201).json({
-        success: true,
-        message: 'API is working',
-        data: specialty
-    })
+app.get("/", async (req: Request, res: Response) => {
+  const specialty = await prisma.specialty.create({
+    data: {
+      title: "Cardiology",
+    },
+  });
+  res.status(201).json({
+    success: true,
+    message: "API is working",
+    data: specialty,
+  });
 });
+
+app.use(globalErrorHandler);
+app.use(notFound);
 
 export default app;
--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -16,36 +16,37 @@ const registerPatient = async (payload: IRegisterPatientPayload) => {
   const { name, email, password } = payload;
 
   const data = await auth.api.signUpEmail({
-    body: {
-      name,
-      email,
-      password,
-      //default values
-      // needsPasswordChange: false,
-      // role: Role.PATIENT
-    },
+    body: { name, email, password },
   });
 
   if (!data.user) {
     throw new Error("Failed to register patient");
   }
 
-  //TODO : Create Patient Profile In Transaction After Sign Up Of Patient In USer Model
-  const patient = await prisma.$transaction(async (tx) => {
-    const patientTx = await tx.patient.create({
-      data: {
-        userId: data.user.id,
-        name: payload.name,
-        email: payload.email,
+  try {
+    const patient = await prisma.$transaction(async (tx) => {
+      return await tx.patient.create({
+        data: {
+          userId: data.user.id,
+          name: payload.name,
+          email: payload.email,
+        },
+      });
+    });
+
+    return {
+      ...data,
+      patient,
+    };
+  } catch (error) {
+    console.log("transaction failed", error);
+    await prisma.user.delete({
+      where: {
+        id: data.user.id,
       },
     });
-    return patientTx;
-  });
-
-  return {
-    ...data,
-    patient,
-  };
+    throw error;
+  }
 };
 
 const loginUser = async (payload: ILoginUserPayload) => {
